f58c7d865c001b2ec3bcf5bfde498f6f1c9fefba
angelika.braun <braun.webdev2024@gmail.com>
2024-06-11 23:01:43 +0200

    create countdown for 2025

---
+++ main.js
@@ -0,0 +1,32 @@
+const currentDate = new Date();
+// console.log(currentDate); // This logs the current date and time to the console
+
+const targetDate = new Date('2025-01-01T00:00:01'); // Replace with target
+// console.log(targetDate); // Check the target date in the console
+
+const difference = targetDate - currentDate;
+// console.log(difference); // This will log the difference in milliseconds
+
+function updateCountdown() {
+    const currentTime = new Date();    
+    const difference = targetDate - currentTime;
+  
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    
+  }
+  const interval = setInterval(updateCountdown, 1000);
+  
+
+  if (difference < 0) {
+    clearInterval(interval);
+    document.getElementById("timer").innerText = "The event has started!";
+  }
